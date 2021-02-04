cf13864eef97549873377e9308dc91b586e0dd1f
SHIKHABAJAJ <64390496+SHIKHABAJAJ@users.noreply.github.com>
2021-02-04 14:22:21 +0530

    Update email slicer logic.py

--- email slicer logic.py
+++ email slicer logic.py
@@ -1,13 +1,3 @@
-##email=input("Enter Your email address :").strip()
-##find=email.index('@')
-##username=email[:find]
-##domain=email[find+1:]
-##print("Username : " ,username)
-##print("Domain   : ", domain)
-
-
-
-
 import regex as re
 email=input("Enter Your email address :").strip()
 s=re.split('@',email)
